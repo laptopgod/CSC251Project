0df1b997920e7cf5a3083f3f6fdb8189cb88c542
Christopher Nguyen <cnguye26@lab.cpcc.edu>
2024-11-10 21:43:04 -0500

    Update Policy.java
    Moved fields and methods to the PolicyHolder

--- Policy.java
+++ Policy.java
@@ -2,33 +2,22 @@ public class Policy //Policy class that will model an insurance policy for one p
 {
    //instance fields
    private int policyNum;
-   private String providerName, firstName, lastName;
-   private int age;
-   private String smokingStatus;
-   private double height; // in inches
-   private double weight; // in pounds
+   private String providerName;
+   
+   //adding a PolicyHolder field   
+   private PolicyHolder policyHolder;
    
    public Policy() //no-arg constructor (set default values for all fields)
    {
       policyNum = 0;
       providerName = "";
-      firstName = "";
-      lastName = "";
-      age = 0;
-      smokingStatus = "non-smoker";
-      height = 0.0;
-      weight = 0.0;
    }
-   public Policy(int pNum, String provName, String fName, String lName, int pAge, String smokeStatus, double pHeight, double pWeight) //constructor that accepts arguments
+   public Policy(int pNum, String provName, PolicyHolder holder) //constructor that accepts arguments
    {
       policyNum = pNum;
       providerName = provName;
-      firstName = fName;
-      lastName = lName;
-      age = pAge;
-      smokingStatus = smokeStatus;
-      height = pHeight;
-      weight = pWeight;
+      policyHolder = holder;
+      policyCount++;
    }
    
    /**
@@ -66,121 +55,7 @@ public class Policy //Policy class that will model an insurance policy for one p
    {
       return providerName;
    }
-   
-   /**
-   Sets the Policyholder’s First Name
-   @param fn the Policyholder’s First Name
-   */
-   public void setFirstName(String fn)
-   {
-      firstName = fn;
-   }
-   
-   /**
-   Returns the Policyholder’s First Name
-   @return The Policyholder’s First Name
-   */
-   public String getFirstName()
-   {
-      return firstName;
-   }
-
-   /**
-   Sets the Policyholder’s Last Name
-   @param ln the Policyholder’s Last Name
-   */
-   public void setLastName(String ln)
-   {
-      lastName = ln;
-   }
-   
-   /**
-   Returns the Policyholder’s Last Name
-   @return The Policyholder’s Last Name
-   */
-   public String getLastName()
-   {
-      return lastName;
-   }
-   
-   /**
-   Sets the Policyholder’s Age
-   @param a the Policyholder’s Age
-   */
-   public void setAge(int a)
-   {
-      age = a;
-   }
-   
-   /**
-   Returns the Policyholder’s Age
-   @return The Policyholder’s Age
-   */
-   public int getAge()
-   {
-      return age;
-   }
-
-   /**
-   Sets the Policyholder’s Smoking Status
-   @param s the Policyholder’s Smoking Status
-   */
-   public void setSmokingStatus(String s)
-   {
-      smokingStatus = s;
-   }
-   
-   /**
-   Returns the Policyholder’s Smoking Status
-   @return The Policyholder’s Smoking Status
-   */
-   public String getSmokingStatus()
-   {
-      return smokingStatus;
-   }
-   
-   /**
-   Sets the Policyholder’s Height (in inches)
-   @param h the Policyholder’s Height
-   */
-   public void setHeight(double h)
-   {
-      height = h;
-   }
-   
-   /**
-   Returns the Policyholder’s Height (in inches)
-   @return The Policyholder’s Height
-   */
-   public double getHeight()
-   {
-      return height;
-   }
-   
-   /**
-   Sets the Policyholder’s Weight (in pounds)
-   @param w the Policyholder’s Weight
-   */
-   public void setWeight(double w)
-   {
-      weight = w;
-   }
-   
-   /**
-   Returns the Policyholder’s Weight (in pounds)
-   @return The Policyholder’s Weight
-   */
-   public double getWeight()
-   {
-      return weight;
-   }
-   
-   //method that calculates and returns the BMI of the policyholder
-   public double calculateBMI()
-   {
-      return (weight * 703) / Math.pow(height, 2);
-   }
-   
+     
    //method that calculates and returns the price of the insurance policy
    public double calculateInsurancePrice()
    {
